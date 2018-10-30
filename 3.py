6c577bf0d23f90660732081612ebdcd3b827e4b3
divyanshmathur1804 <44617450+divyanshmathur1804@users.noreply.github.com>
2018-10-30 23:44:21 +0530

    3.py
    Printing that name backwards

---
+++ 3.py
@@ -0,0 +1,15 @@
+Python 2.6.6 (r266:84297, Aug 24 2010, 18:13:38) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+
+    ****************************************************************
+    Personal firewall software may warn about the connection IDLE
+    makes to its subprocess using this computer's internal loopback
+    interface.  This connection is not visible on any external
+    interface and no data is sent to or received from the Internet.
+    ****************************************************************
+    
+IDLE 2.6.6      ==== No Subprocess ====
+>>> 
+Enter your name:divyansh
+Hello divyansh pleased to meet you! Did you know that your name backwards is hsnayvid
+>>> 
